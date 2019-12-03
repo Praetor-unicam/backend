db141da4aa398e1270d68820bdd71be21824aff2
Michele Biondi <michelebiondi01@gmail.com>
2019-12-03 12:21:33 +0100

    Improved download ad add checkstatus

--- src/scraper/bulgary.ts
+++ src/scraper/bulgary.ts
@@ -1,16 +1,29 @@
 import download from 'download';
+import * as fs from 'fs';
 
 const country = 'Bulgary';
+const filename = country + 'xlsx';
 
 const startUrl = 'https://www.nsi.bg/sites/default/files/files/data/timeseries/JST_1.2_en.xls';
 const downloadDir = String(process.env.DATA_DOWNLOAD_DIR);
 
 export const isServiceAvailable = async (): Promise<boolean> => {
+    try {
+        await download(startUrl);
+    } catch (err) {
+        return false;
+    }
+
     return true;
 };
 
-export const downloadData = async (): Promise<void> => {
-    await download(startUrl, downloadDir);
-};
+export const downloadData = async (): Promise<boolean> => {
+    try {
+        await download(startUrl, downloadDir);
+        fs.writeFileSync(downloadDir + '/' + filename, await download(startUrl));
+    } catch (err) {
+        return false;
+    }
 
-downloadData();
+    return true;
+};
