a486c81bac8e665b7d93389e743e89318e58986d
Michele Biondi <michelebiondi01@gmail.com>
2019-12-12 11:10:15 +0100

    add bulgary file as template

---
+++ src/routes/scraper/cyprus.ts
@@ -0,0 +1,36 @@
+import download from 'download';
+import * as fs from 'fs';
+import { promisify } from 'util';
+
+const writeFile = promisify(fs.writeFile);
+const removeFile = promisify(fs.unlink);
+
+const country = 'Bulgary';
+const filename = country + '.xlsx';
+
+const expectedFilename = 'JST_1.2_en.xls';
+
+const startUrl = 'https://www.nsi.bg/sites/default/files/files/data/timeseries/' + expectedFilename;
+const downloadDir = String(process.env.DATA_DOWNLOAD_DIR);
+
+export const isServiceAvailable = async (): Promise<boolean> => {
+    try {
+        await download(startUrl);
+    } catch (err) {
+        return false;
+    }
+
+    return true;
+};
+
+export const downloadData = async (): Promise<boolean> => {
+    try {
+        await download(startUrl, downloadDir);
+        await writeFile(downloadDir + '/' + filename, await download(startUrl));
+        await removeFile(downloadDir + '/' + expectedFilename);
+    } catch (err) {
+        return false;
+    }
+
+    return true;
+};
