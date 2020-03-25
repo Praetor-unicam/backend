5ba2b8bb2d8352b5225ab0985dde6bfab3ff6b2e
manuel cretone <manuel.cretone@studenti.unicam.it>
2020-03-25 14:49:07 +0100

    adding comments

--- src/routes/api_calls.ts
+++ src/routes/api_calls.ts
@@ -1,10 +1,7 @@
 import { Country } from '../loader';
-//import { loader } from './../loader';
 import express, { response } from 'express';
 import { ICountry } from '../interfaces/Country';
-import { Comparation } from '../interfaces/Comparation.interface';
 import { Crime } from '../interfaces/crime.interface';
-import { LocalizedData } from '../interfaces/LocalizedData.interface';
 import { compare } from '../comparator';
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -13,10 +10,11 @@ const Country = require('../model/State');
 const Maps = require('../model/Maps');
 const fs = require('fs');
 const ICountry = require('../interfaces/Country');
-
 const read = require('../fileService');
 
-// create instance of a luxembourg state
+/**
+ *  create instance of a state with all data crimes
+ *  */
 router.post('/create/:country', async (req, res) => {
     try {
         const country = req.params.country;
@@ -26,7 +24,6 @@ router.post('/create/:country', async (req, res) => {
             .save()
             .then((result: any) => {
                 res.status(201).json(result);
-                console.log(result);
             })
             .catch((err: any) => console.log(err));
         res.status(201).json({
@@ -37,7 +34,9 @@ router.post('/create/:country', async (req, res) => {
         console.log(error);
     }
 });
-
+/**
+ * returns the years in which crime data exist
+ */
 router.get('/year_available/:country/:ID', async (req, res) => {
     const country = req.params.country;
     const id = req.params.id;
@@ -105,7 +104,6 @@ router.get('/years/:country/:nuts_id', async (req, res) => {
         .exec()
         .then((doc: any[]) => {
             var years: any[] = [];
-            console.log(doc);
             doc.forEach(element => {
                 //let new_data = { year: element.year.year };
                 years.push(element.year.year);
@@ -159,7 +157,6 @@ async function db_call(country: String, nuts: any, array: any[]): Promise<any> {
     await Country.aggregate(query)
         .exec()
         .then((doc: any[]) => {
-            console.log(doc.length);
             if (doc.length != 0) {
                 let res = { NUTS: nuts, available: true };
                 array.push(res);
@@ -173,9 +170,11 @@ async function db_call(country: String, nuts: any, array: any[]): Promise<any> {
         .catch((err: any) => {
             console.log(err);
         });
-    console.log(array);
 }
 
+/**
+ * return all data of a specific country
+ */
 router.get('/data/:country/:ID', async (req, res) => {
     const country = req.params.country;
     const year = req.query.year;
@@ -219,12 +218,14 @@ router.get('/data/:country/:ID', async (req, res) => {
         });
 });
 
-//return all possible iccs_crime in each data place
+/**
+ * return all possible iccs_crime in each data place
+ */
+
 router.get('/data/ICCS_category', async (req, res) => {
     await Country.distinct('year.place.data.ICCS_crime')
         .exec()
         .then((doc: any[]) => {
-            //array.push(doc[0].data)
             res.status(200).json(doc);
         })
         .catch((err: any) => {
@@ -232,9 +233,10 @@ router.get('/data/ICCS_category', async (req, res) => {
         });
 });
 
-// call to compare iccs data
+/**
+ * call to compare ICCS data crimes
+ */
 async function compare_data(countries: string, locations: string, year: string): Promise<Array<Crime>> {
-    // const output: Comparation = { countries: [] };
     const crimes: Array<Crime> = [];
     const c = countries;
     const y = year;
@@ -271,91 +273,34 @@ async function compare_data(countries: string, locations: string, year: string):
         .exec()
         .then((doc: any[]) => {
             crimes.push(doc[0].data);
-            //return doc;
         })
         .catch((err: any) => {
             return err;
         });
-    console.log(crimes);
+
     return crimes;
 }
-
+/**
+ * comparison of multi-state data crimes
+ */
 router.get('/dat/compare/:year', async (req, res) => {
     const country = JSON.parse(req.query.country);
     const nuts = JSON.parse(req.query.nuts);
-    console.log(country);
-
     const year = req.params.year;
-
     const crimes: Array<Array<Crime>> = [];
     const output: any = [];
-
     let result: any = {};
     let i = 0;
     for (const element of country) {
-        console.log(element);
         crimes.push(await compare_data(element, nuts[i], year));
 
         i++;
     }
-    //res.json(crimes[1][0]);
     res.json(await compare(country, nuts, year, crimes));
 });
-
-// compare iccs data, return all data crimes
-// router.get('/data/compare/:country/:year/:ID', async (req, res) => {
-//     const country = req.params.country;
-//     const year = req.params.year;
-//     const ID = req.params.ID;
-//     const array = ['LU', 'IT'];
-//     const paese = ['LU', 'ITI1'];
-
-//     const query = [
-//         {
-//             $match: {
-//                 CountryNUTS: country,
-//             },
-//         },
-//         {
-//             $unwind: '$year',
-//         },
-//         {
-//             $match: {
-//                 'year.year': year,
-//             },
-//         },
-//         {
-//             $unwind: '$year.place',
-//         },
-//         {
-//             $match: {
-//                 'year.place.NUTS': ID,
-//             },
-//         },
-//         {
-//             $project: {
-//                 countries: [
-//                     {
-//                         country: country,
-//                         location: ID,
-//                         year: year,
-//                         data: '$year.place.data',
-//                     },
-//                 ],
-//             },
-//         },
-//     ];
-
-//     await Country.aggregate(query)
-//         .exec()
-//         .then((doc: any[]) => {
-//             res.status(200).json(doc[0]);
-//         })
-//         .catch((err: any) => {
-//             res.status(500).json(err);
-//         });
-// });
-
+/**
+ * check the availability of data for a country
+ */
 router.get('/available_data/:country/:ID', async (req, res) => {
     const id = req.params.ID;
     const country = req.params.country;
@@ -388,7 +333,6 @@ router.get('/available_data/:country/:ID', async (req, res) => {
         .then((doc: any[]) => {
             if (doc.length != 0) {
                 res.status(200).json(doc);
-                console.log(doc);
                 return true;
             }
         })
@@ -396,7 +340,9 @@ router.get('/available_data/:country/:ID', async (req, res) => {
             res.status(500).json(err);
         });
 });
-
+/**
+ * insertion map in the database specifying the id that identifies the state
+ */
 router.post('/maps/:ID', async (req, res) => {
     const id = req.params.ID;
     const maps = read.recFindByExt('maps/' + id, 'geojson');
@@ -441,321 +387,5 @@ router.get('/map/:ID', async (req, res) => {
             res.status(500).json(err);
         });
 });
-
-// router.get('/data/:country_nuts', async (req, res) => {
-//     const nuts_state = req.params.country_nuts;
-
-//     await Country.find({ NUTS_ID: nuts_state })
-//         .exec()
-//         .then((doc: any) => {
-//             res.status(200).json(doc);
-//         })
-//         .catch((err: any) => {
-//             res.status(500).json(err);
-//         });
-// });
-
-// router.get('/data/:year/:country_nuts/:region_nuts', async (req, res) => {
-//     const nuts_state = req.params.country_nuts;
-//     const nuts_region = req.params.region_nuts;
-//     const year = req.params.year;
-//     const query = [
-//         {
-//             $match: {
-//                 NUTS_ID: nuts_state,
-//             },
-//         },
-//         {
-//             $unwind: '$year',
-//         },
-//         {
-//             $match: {
-//                 'year.year': year,
-//             },
-//         },
-//         {
-//             $unwind: '$year.region',
-//         },
-//         {
-//             $match: {
-//                 'year.region.NUTS': nuts_region,
-//             },
-//         },
-//         {
-//             $project: {
-//                 crime: '$year.region.data.crime',
-//                 value: '$year.region.data.value',
-//             },
-//         },
-//     ];
-
-//     await Country.aggregate(query)
-//         .exec()
-//         .then((doc: any) => {
-//             res.status(200).json(doc);
-//         })
-//         .catch((err: any) => {
-//             res.status(500).json(err);
-//         });
-// });
-
-// //if specified country return all documents
-// router.get('/data/:country', async (req, res) => {
-//     const state = req.params.country;
-//     console.log(state);
-//     await Country.find({ country: state })
-//         .exec()
-//         .then((doc: any) => {
-//             res.status(200).json(doc);
-//         })
-//         .catch((err: any) => {
-//             res.status(500).json(err);
-//         });
-// });
-
-// // read all documents that state name is specified in request param
-// router.get('/data/:country/:year', async (req, res) => {
-//     const state = req.params.country;
-//     const year = parseInt(req.params.year);
-//     console.log(state);
-//     console.log(year);
-//     const query = [
-//         {
-//             $match: {
-//                 county: state,
-//             },
-//         },
-//         {
-//             $unwind: '$year',
-//         },
-//         {
-//             $match: {
-//                 'year.year': year,
-//             },
-//         },
-//         {
-//             $unwind: '$year.region',
-//         },
-//         {
-//             $unwind: '$year.region.province',
-//         },
-
-//         {
-//             $unwind: '$year.region.province.county',
-//         },
-//         {
-//             $unwind: '$year.region.province.county.data',
-//         },
-//         {
-//             $project: {
-//                 crime: '$year.region.province.county.data.crime',
-//                 value: '$year.region.province.county.data.value',
-//             },
-//         },
-//     ];
-
-//     await State.aggregate(query)
-//         .exec()
-//         .then(doc => {
-//             console.log(doc);
-//             crimes = [];
-//             //return doc;
-//             doc.forEach(crime => {
-//                 let new_crime = { name: crime.crime, n_crimes: parseInt(crime.value) };
-//                 crimes.push(new_crime);
-//             });
-//             res.status(200).json(crimes);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         });
-// });
-
-// router.get('/data/:country/:year/:region', async (req, res) => {
-//     const state = req.params.country;
-//     const year = parseInt(req.params.year);
-//     const region = req.params.region;
-//     console.log(state);
-//     console.log(year);
-//     const query = [
-//         {
-//             $match: {
-//                 country: state,
-//             },
-//         },
-//         {
-//             $unwind: '$year',
-//         },
-//         {
-//             $match: {
-//                 'year.year': year,
-//             },
-//         },
-//         {
-//             $unwind: '$year.region',
-//         },
-//         {
-//             $match: {
-//                 'year.region.region': region,
-//             },
-//         },
-//         {
-//             $unwind: '$year.region.province',
-//         },
-
-//         {
-//             $unwind: '$year.region.province.data',
-//         },
-//         {
-//             $project: {
-//                 crime: '$year.region.province.data.crime',
-//                 value: '$year.region.province.data.value',
-//                 code: '$year.region.province.data.code',
-//             },
-//         },
-//     ];
-
-//     await Country.aggregate(query)
-//         .exec()
-//         .then((doc: any) => {
-//             console.log(doc);
-//             const crimes: any = [];
-//             //return doc;
-//             doc.forEach((crime: any) => {
-//                 let new_crime = { name: crime.crime, n_crimes: parseInt(crime.value) };
-//                 crimes.push(new_crime);
-//             });
-//             res.status(200).json(crimes);
-//         })
-//         .catch((err: any) => {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         });
-// });
-
-// // return all categories of ICCS at level of country
-// router.get('/categories/:country', async (req, res) => {
-//     const state = req.params.country;
-//     await State.find({ country: state })
-//         .distinct('year.region.province.county.data.crime')
-//         .exec()
-//         .then(doc => {
-//             console.log(doc);
-//             crimes = [];
-//             doc.forEach(crime => {
-//                 let new_crime = { name: crime };
-
-//                 // const new_crime = { name: crime.crime, n_crimes: parseInt(crime.value) };
-
-//                 crimes.push(new_crime);
-//             });
-//             res.status(200).json(crimes);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         });
-// });
-
-// router.get('/categories/countries', async (req, res) => {
-//     await State.distinct('year.data.crime')
-//         .exec()
-//         .then(doc => {
-//             console.log(doc);
-//             crimes = [];
-//             doc.forEach(crime => {
-//                 let new_crime = { name: crime };
-//                 crimes.push(new_crime);
-//             });
-//             res.status(200).json(crimes);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         });
-// });
-
-// //return boolean value if country data are presents
-// router.get('/data_status/:country', async (req, res) => {
-//     const state = req.params.country;
-//     await State.where('country', state)
-//         .countDocuments()
-//         .exec()
-//         .then((docs, err) => {
-//             if (docs > 0) {
-//                 res.status(200).json(true);
-//                 return true;
-//             } else {
-//                 res.status(200).json(false);
-//                 return false;
-//             }
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         });
-// });
-
-// //return boolean value if region data are presents
-// router.get('/data_status/:country/:region', async (req, res) => {
-//     const state = req.params.country;
-//     const region = req.params.region;
-//     await State.aggregate([
-//         { $match: { country: state } },
-//         { $unwind: '$year' },
-//         { $unwind: '$year.region' },
-//         { $match: { 'year.region.region': region } },
-//         { $unwind: '$year.region.province' },
-//     ])
-//         .exec()
-//         .then((docs, err) => {
-//             console.log(docs.length);
-//             if (docs.length > 0) {
-//                 res.status(200).json(true);
-//                 return true;
-//             } else {
-//                 res.status(200).json(false);
-//                 return false;
-//             }
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         });
-// });
-
-// router.get('/data_status/:country/:region/:province', async (req, res) => {
-//     const state = req.params.country;
-//     const region = req.params.region;
-//     console.log(region);
-//     const province = req.params.province;
-//     console.log(province);
-
-//     await State.aggregate([
-//         { $match: { country: state } },
-//         { $unwind: '$year' },
-//         { $unwind: '$year.region' },
-//         { $match: { 'year.region.region': region } },
-//         { $unwind: '$year.region.province' },
-//         { $match: { 'year.region.province.province': province } },
-//         { $unwind: '$year.region.province.county' },
-//         { $unwind: '$year.region.province.county.data' },
-//     ])
-//         .exec()
-//         .then((docs, err) => {
-//             if (docs.length > 0) {
-//                 res.status(200).json(true);
-//                 return true;
-//             } else {
-//                 res.status(200).json(false);
-//                 return false;
-//             }
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         });
-// });
 
 module.exports = router;
